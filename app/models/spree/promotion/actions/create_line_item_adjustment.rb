215118eef35c16f2acb29e5d7a5071372efc194e
Kunwar Aditya Raghuwanshi <adi.version1@gmail.com>
2016-04-22 10:18:00 -0700

    use try for complete check as it would fail on line items

--- app/models/spree/promotion/actions/create_line_item_adjustment.rb
+++ app/models/spree/promotion/actions/create_line_item_adjustment.rb
@@ -85,7 +85,7 @@ module Spree
 
         def deals_with_adjustments
           self.adjustments.each do |adjustment|
-            if adjustment.source.complete?
+            if adjustment.source.try(:complete?)
               adjustment.originator = nil
               adjustment.save
             else
